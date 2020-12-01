10332ce4e3da1b93e5f82fa692cf329a07c9a67c
Guilherme Higa <guilhermehiga11@gmail.com>
2020-12-01 16:44:04 -0300

    Projeto Criado

---
+++ src/Application/Program.java
@@ -0,0 +1,13 @@
+package Application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		System.out.println("Digite seu nome : ");
+		String nome = ler.nextLine();
+	}
+
+}
